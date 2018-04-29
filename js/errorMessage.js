65929a1ad91da5f658cd92b3351a8861467fd263
Elizaveta <elizabeth11@bk.ru>
2018-04-29 17:22:16 +0300

    refactoring errormessage.js

--- js/errorMessage.js
+++ js/errorMessage.js
@@ -1,21 +1,33 @@
 'use strict';
 
 window.errorMessage = (function () {
+  var SHOW_ERROR_TIMEOUT = 6000;
+
+  var errorDataElement = document.createElement('div');
+
+  errorDataElement.style.backgroundColor = 'rgba(255, 0, 0, 0.8)';
+  errorDataElement.style.color = '#000000';
+  errorDataElement.style.textAlign = 'center';
+  errorDataElement.style.margin = '0 auto';
+  errorDataElement.style.position = 'fixed';
+  errorDataElement.style.left = 0;
+  errorDataElement.style.right = 0;
+  errorDataElement.style.fontSize = '30px';
+  errorDataElement.style.zIndex = '100';
+
+  document.body.insertAdjacentElement('afterbegin', errorDataElement);
+
   return {
     show: function (errorMessage) {
-      var errorDataElement = document.createElement('div');
-      errorDataElement.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: rgba(255, 0, 0, 0.8); color: #000000;';
-      errorDataElement.style.position = 'fixed';
-      errorDataElement.style.left = 0;
-      errorDataElement.style.right = 0;
-      errorDataElement.style.fontSize = '30px';
+      if (errorDataElement.classList.contains('hidden')) {
+        errorDataElement.classList.remove('hidden');
+      }
 
       errorDataElement.textContent = errorMessage;
-      document.body.insertAdjacentElement('afterbegin', errorDataElement);
 
       setTimeout(function () {
         errorDataElement.classList.add('hidden');
-      }, 6000);
+      }, SHOW_ERROR_TIMEOUT);
     }
   };
 })();
